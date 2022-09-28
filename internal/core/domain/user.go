0b3cecc59e3b3ce031f8e6fe678cf32924d0729f
Breno Oliveira <soubreno@gmail.com>
2022-09-28 15:01:47 +0100

    main - Update user domain

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -1,6 +1,10 @@
 package domain
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
 
 type User struct {
 	ID         string `json:"id"`
@@ -8,6 +12,11 @@ type User struct {
 	Token      string `json:"token,omitempty"`
 	IsPaidUser bool   `json:"isPaidUser,omitempty"`
 	RequestQtt int32  `json:"requestQtt,omitempty"`
+}
+
+type CreateUserRequest struct {
+	Email      string `json:"email"`
+	IsPaidUser bool   `json:"isPaidUser"`
 }
 
 func NewUser(id string, email string, token string, isPaidUser bool, requestQtt int32) *User {
@@ -22,4 +31,13 @@ func NewUser(id string, email string, token string, isPaidUser bool, requestQtt 
 
 func (u *User) String() string {
 	return fmt.Sprintf("%s - %s - Paid: %t", u.ID, u.Email, u.IsPaidUser)
+}
+
+func FromJSONCreateUserRequest(body io.Reader) (*CreateUserRequest, error) {
+	createUserRequest := CreateUserRequest{}
+	if err := json.NewDecoder(body).Decode(&createUserRequest); err != nil {
+		return nil, err
+	}
+
+	return &createUserRequest, nil
 }
